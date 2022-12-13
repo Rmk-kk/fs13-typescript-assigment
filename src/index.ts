a822cbc41517b04bc18a751fa4a9b13da0e5e824
Roman <demianchuk.ro@gmail.com>
2022-12-13 17:47:34 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import {Branch} from "./Branch";
+
+class Bank  {
+    name;
+    branches: Branch[];
+    constructor(name: string) {
+        this.name = name;
+        this.branches = [];
+    }
+
+    addBranch() {
+
+    }
+    addCustomer() {
+
+    }
+
+    addCustomerTransaction() {
+
+    }
+
+    findBranchByName() {
+
+    }
+
+    checkBranch() {
+
+    }
+
+    listCustomers() {
+
+    }
+}
+
