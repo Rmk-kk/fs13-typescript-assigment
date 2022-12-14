6c34cc97ad4d10ba3cc270c8bd5367b172cf7bfc
Roman <demianchuk.ro@gmail.com>
2022-12-14 07:26:49 +0200

    refactoring code

--- build/index.js
+++ build/index.js
@@ -1,33 +1,40 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Branch_1 = require("./Branch");
-const Customer_1 = require("./Customer");
-const Bank_1 = require("./Bank");
-const arizonaBank = new Bank_1.Bank("Arizona");
-const westBranch = new Branch_1.Branch("West Branch");
-const sunBranch = new Branch_1.Branch("Sun Branch");
-const customer1 = new Customer_1.Customer("John");
-const customer2 = new Customer_1.Customer("Anna");
-const customer3 = new Customer_1.Customer("John");
-console.log(`START`);
-arizonaBank.addBranch(westBranch);
-arizonaBank.addBranch(sunBranch);
-arizonaBank.addBranch(westBranch);
-console.log(`--------`);
-arizonaBank.findBranchByName("bank");
-arizonaBank.findBranchByName("sun");
-arizonaBank.addCustomer(westBranch, customer1);
-arizonaBank.addCustomer(westBranch, customer3);
-arizonaBank.addCustomer(sunBranch, customer1);
-arizonaBank.addCustomer(sunBranch, customer2);
+import {Branch} from "./Branch";
+import {Customer} from "./Customer";
+import { Bank } from "./Bank";
+
+
+const arizonaBank = new Bank("Arizona")
+const westBranch = new Branch("West Branch")
+const sunBranch = new Branch("Sun Branch")
+const customer1 = new Customer("John")
+
+const customer2 = new Customer("Anna")
+
+const customer3 = new Customer("John")
+
+
+console.log(`START`)
+arizonaBank.addBranch(westBranch)
+arizonaBank.addBranch(sunBranch)
+arizonaBank.addBranch(westBranch)
+console.log(`--------`)
+arizonaBank.findBranchByName("bank")
+arizonaBank.findBranchByName("sun")
+
+arizonaBank.addCustomer(westBranch, customer1)
+arizonaBank.addCustomer(westBranch, customer3)
+arizonaBank.addCustomer(sunBranch, customer1)
+arizonaBank.addCustomer(sunBranch, customer2)
 // //
-console.log(`--------`);
-arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 3000);
-arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 2000);
-arizonaBank.addCustomerTransaction(westBranch, customer2.getId(), 3000);
-customer1.addTransaction(-1000);
+console.log(`--------`)
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 3000)
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 2000)
+arizonaBank.addCustomerTransaction(westBranch, customer2.getId(), 3000)
+
+customer1.addTransaction(-1000)
 customer1.getBalance();
-console.log(`--------`);
-arizonaBank.listCustomers(westBranch, true);
-arizonaBank.listCustomers(sunBranch, true);
-console.log('END');
+console.log(`--------`)
+arizonaBank.listCustomers(westBranch, true)
+arizonaBank.listCustomers(sunBranch,true)
+console.log('END')
